ee4fe12975a8b0edfcbd12a8593d6f6426af6ffd
Thainan Esteves <thainan239@gmail.com>
2023-06-19 17:40:59 -0300

    Create Seller entitie

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1,"Recursos Humanos");
+        Seller seller = new Seller(1,"Thainan","thainan@gmail.com",new Date(),4000.0,department);
 
-        System.out.println(department);
+        System.out.println(seller);
     }
 }
