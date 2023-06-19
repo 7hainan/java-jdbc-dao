74ddea5482b22e1ca2163b261a2408de25404cc1
Thainan Esteves <thainan239@gmail.com>
2023-06-19 17:55:53 -0300

    Create dao factory and dao interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Department department = new Department(1,"Recursos Humanos");
         Seller seller = new Seller(1,"Thainan","thainan@gmail.com",new Date(),4000.0,department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
